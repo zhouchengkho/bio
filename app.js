c85398a1042ca8511465e08c8e9fc3230b04528d
root <741713939@qq.com>
2018-08-16 19:02:01 +0000

    add https

--- app.js
+++ app.js
@@ -2,19 +2,32 @@
 
 var express = require('express'),
   config = require('./config/config'),
-  db = require('./app/models');
+  db = require('./app/models'),
+  fs = require('fs'),
+  http = require('http'),
+  https = require('https');
+
 
 var app = express();
+const privateKey = fs.readFileSync('/https_cert/server.key');
+const cert = fs.readFileSync('/https_cert/server.crt');
+const credentials = { key: privateKey, cert: cert };
 
+let httpServer = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+          
 module.exports = require('./config/express')(app, config);
 
 db.sequelize
   .sync()
   .then(function () {
     if (!module.parent) {
-      app.listen(config.port, function () {
-        console.log('Express server listening on port ' + config.port);
-      });
+         httpServer.listen(config.port, (err) => {
+             console.log('http started at' + config.port);
+         });
+         httpsServer.listen(config.httpsPort, (err) => {
+             console.log('https started at ' + config.httpsPort);
+         });
     }
   }).catch(function (e) {
     throw new Error(e);
